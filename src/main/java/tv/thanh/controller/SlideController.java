6c647f6a11c48f57d90b408f86e8743d37e57809
bk-ducle <leanhduc2015@gmail.com>
2017-10-23 02:25:09 +0700

    them , cap nhap, xoa slide

--- src/main/java/tv/thanh/controller/SlideController.java
+++ src/main/java/tv/thanh/controller/SlideController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import tv.thanh.model.Slide;
+import tv.thanh.repository.SlideRepository;
 import tv.thanh.service.SlideService;
 
 @RestController
@@ -16,15 +21,37 @@ import tv.thanh.service.SlideService;
 public class SlideController {
 	@Autowired
 	private SlideService slideService;
-	
-	@RequestMapping(value="", method=RequestMethod.GET)
-	public List<Slide> getAll(){
+
+	@Autowired
+	private SlideRepository slideRepository;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<Slide> getAll() {
 		return (List<Slide>) slideService.findAll();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Slide getById(@PathVariable("id") int id) {
 		return slideService.findById(id);
 	}
-}
 
+	@PostMapping("/add")
+	public Slide addSlide(@RequestBody Slide slide) {
+		return slideRepository.save(slide);
+	}
+
+	@PostMapping("/update")
+	public Slide updateSlide(@RequestBody Slide slide) {
+		return slideRepository.save(slide);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteById(@PathVariable("id") int id) {
+		Slide slide = slideRepository.findOne(id);
+		if (slide == null) {
+			return "not found";
+		}
+		slideRepository.delete(slide);
+		return "success";
+	}
+}
