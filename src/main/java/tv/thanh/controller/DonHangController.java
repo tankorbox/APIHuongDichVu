df9b470cb4a6d915130c1463b04812276b5456e4
bk-ducle <leanhduc2015@gmail.com>
2017-10-23 14:48:31 +0700

    change donhangcontroller

--- src/main/java/tv/thanh/controller/DonHangController.java
+++ src/main/java/tv/thanh/controller/DonHangController.java
@@ -2,6 +2,7 @@ package tv.thanh.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import tv.thanh.model.ChiTietDonHang;
 import tv.thanh.model.DonHang;
+import tv.thanh.repository.ChiTietDonHangRepository;
 import tv.thanh.repository.DonHangRepository;
 import tv.thanh.service.DonHangService;
 
@@ -19,55 +22,75 @@ import tv.thanh.service.DonHangService;
 public class DonHangController {
 	@Autowired
 	private DonHangService donHangService;
-	
+
 	@Autowired
 	private DonHangRepository donHangRepository;
-	
-	@RequestMapping(value="", method=RequestMethod.GET)
-	public List<DonHang> getAll(){
+
+	@Autowired
+	private ChiTietDonHangRepository chitietdonhangRepository;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<DonHang> getAll() {
 		return (List<DonHang>) donHangService.findAll();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public DonHang getById(@PathVariable("id") int id) {
 		return donHangService.findById(id);
 	}
-	
-	@RequestMapping(value="/getbykhachhangid/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/getbykhachhangid/{id}", method = RequestMethod.GET)
 	public List<DonHang> getDonHangByKhachHangId(@PathVariable("id") int id) {
-		List<DonHang> donHangs = donHangService.findAll();	
+		List<DonHang> donHangs = donHangService.findAll();
 		List<DonHang> ketqua = new ArrayList<>();
 		for (DonHang donHang : donHangs) {
-			if (donHang.getNguoidung().getId()==id) ketqua.add(donHang);
+			if (donHang.getNguoidung().getId() == id)
+				ketqua.add(donHang);
 		}
 		return ketqua;
 	}
-	
-	@RequestMapping(value="/getbytrangthai/{trangthai}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/getbytrangthai/{trangthai}", method = RequestMethod.GET)
 	public List<DonHang> getDonHangByTinhTrang(@PathVariable("trangthai") int trangthai) {
-		List<DonHang> donHangs = donHangService.findAll();	
+		List<DonHang> donHangs = donHangService.findAll();
 		List<DonHang> ketqua = new ArrayList<>();
 		for (DonHang donHang : donHangs) {
-			if (donHang.getTrangthai()==trangthai) ketqua.add(donHang);
+			if (donHang.getTrangthai() == trangthai)
+				ketqua.add(donHang);
 		}
 		return ketqua;
 	}
-	
-	@RequestMapping(value="/add",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public DonHang addDonHang(@RequestBody DonHang donhang) {
 		return donHangRepository.save(donhang);
 	}
-	
-	@RequestMapping(value="/update", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public DonHang updateDonHang(@RequestBody DonHang donhang) {
 		return donHangRepository.save(donhang);
 	}
-	
-	@RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
 	public String deleteDonHang(@PathVariable("id") int id) {
 		DonHang dh = donHangRepository.findOne(id);
-		if (dh==null) return "not found";
+		if (dh == null)
+			return "not found";
 		donHangRepository.delete(id);
 		return "success";
+	}
+
+	@RequestMapping(value = "/add2", method = RequestMethod.POST)
+	public DonHang addDonHang2(@RequestBody DonHang donhang) {
+		Set<ChiTietDonHang> ctdh = donhang.getChiTietDonHangs();
+		System.out.println(ctdh.toString());
+		Set<ChiTietDonHang> ctdh2 = null;
+		for (ChiTietDonHang chitiet : ctdh) {
+			chitiet.setId(0);
+			ChiTietDonHang ct = chitietdonhangRepository.save(chitiet);
+			ctdh2.add(ct);
+		}
+		donhang.setChiTietDonHangs(ctdh2);
+		return donHangRepository.save(donhang);
 	}
 }
